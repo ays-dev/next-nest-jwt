0bf0e474c444e5c2f4c0d96e1f07e5f0e5446b8e
Abed <abed@macbook-pro.home>
2025-01-22 22:55:12 +0100

    Update tests

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,25 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+  let server: any;
+  let token: string;
 
-  beforeEach(async () => {
+  const testEmail = `e2e-user-${Date.now()}@example.com`;
+  const testPassword = 'testPassword123';
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    server = app.getHttpServer();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Register a new user (POST /auth/register)', async () => {
+    const response = await request(server)
+      .post('/auth/register')
+      .send({ email: testEmail, password: testPassword })
+      .expect(201);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('email', testEmail);
+  });
+
+  it('Login with user credentials (POST /auth/login)', async () => {
+    const response = await request(server)
+      .post('/auth/login')
+      .send({ email: testEmail, password: testPassword })
+      .expect(201);
+
+    expect(response.body).toHaveProperty('access_token');
+    token = response.body.access_token;
+  });
+
+  it('Get current user profile (GET /auth/profile)', async () => {
+    const response = await request(server)
+      .get('/auth/profile')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+
+      expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('email', testEmail);
   });
 });
